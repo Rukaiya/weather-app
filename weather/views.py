dba110ef0d5b440651861e18c7b13c1ca74133ff
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-10-25 07:30:20 +0600

    weather context completed

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,23 @@
+from urllib import response
+import requests
 from django.shortcuts import render
 from decouple import config
 # Create your views here.
 
 def index(request):
     APP_KEY = config('APP_KEY')
-    url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&appid={APP_KEY}&units=metric'
+    print(APP_KEY)
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units={}'
     city = 'Dhaka'
-    return render(request, 'weather/main.html')
+    units = 'metric'
+    response = requests.get(url.format(city, APP_KEY, units)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': response['main']['temp'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    context = {'city_weather': city_weather}
+    return render(request, 'weather/main.html', context)
