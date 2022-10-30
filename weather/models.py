6f48853a1c6fbdb9632a5cd820d6fe4c99a77a1a
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-10-30 08:00:12 +0600

    ModelForm for Add City button  23.49

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,13 @@
+from operator import mod
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Cities"
--- weather/views.py
+++ weather/views.py
@@ -1,23 +1,35 @@
+import imp
 from urllib import response
 import requests
 from django.shortcuts import render
 from decouple import config
+from .models import City
+from .forms import CityForm
 # Create your views here.
 
 def index(request):
     APP_KEY = config('APP_KEY')
     print(APP_KEY)
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units={}'
-    city = 'Dhaka'
     units = 'metric'
-    response = requests.get(url.format(city, APP_KEY, units)).json()
 
-    city_weather = {
-        'city': city,
-        'temperature': response['main']['temp'],
-        'description': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
-    }
+    if request.method == 'POST':
+        print(request.POST)
 
-    context = {'city_weather': city_weather}
+    form = CityForm()
+    weather_data = []
+    cities = City.objects.all()
+    
+    for city in cities:
+        response = requests.get(url.format(city, APP_KEY, units)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': response['main']['temp'],
+            'description': response['weather'][0]['description'],
+            'icon': response['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+    print(weather_data)
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/main.html', context)
